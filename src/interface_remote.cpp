6abdb3fb37b80b4026981bc175272a1e6f175ab1
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-22 16:37:26 -0700

    Changing QoS settings to enable intraprocess communications

--- src/interface_remote.cpp
+++ src/interface_remote.cpp
@@ -50,7 +50,7 @@ void RemoteInterface::get_clnt_position_set_() {
         .depth = 1,
         .reliability = rmw_qos_reliability_policy_t::
             RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
-        .durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+        .durability = RMW_QOS_POLICY_DURABILITY_VOLATILE,
         .deadline = {0, 50000000},
         .lifespan = {0, 50000000},
         .liveliness = RMW_QOS_POLICY_LIVELINESS_AUTOMATIC,
@@ -101,7 +101,7 @@ void RemoteInterface::get_clnt_velocity_set_() {
         .depth = 1,
         .reliability = rmw_qos_reliability_policy_t::
             RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
-        .durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+        .durability = RMW_QOS_POLICY_DURABILITY_VOLATILE,
         .deadline = {0, 50000000},
         .lifespan = {0, 50000000},
         .liveliness = RMW_QOS_POLICY_LIVELINESS_AUTOMATIC,
